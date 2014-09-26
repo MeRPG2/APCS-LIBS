26f81ca8da51861817d6995ed4c60767023b0f23
Jaxon <MeRPG@users.noreply.github.com>
2014-09-26 12:07:54 -0400

    Added the printPicture(String path) method.

--- IO.java
+++ IO.java
@@ -27,4 +27,39 @@ public class IO {
 		} catch(Exception e) {e.printStackTrace();}
 		return null;
 	}
+	public static void printPicture(String path) {
+		try {
+			BufferedImage in = ImageIO.read(new File(path));
+			BufferedImage imgUnscled = new BufferedImage(in.getWidth(), in.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+			Graphics2D g = imgUnscled.createGraphics();
+			g.drawImage(in, 0, 0, null);
+			g.setBackground(Color.white);
+			g.dispose();
+			BufferedImage img = scale(imgUnscled, BufferedImage.TYPE_4BYTE_ABGR, 125, 50, 125D/imgUnscled.getWidth(), 50D/imgUnscled.getHeight());
+			int c = 0;
+			for(int y = 0; y < img.getHeight(); y++) {
+				for(int x = 0; x < img.getWidth(); x++) {
+					c = img.getRGB(x, y);
+					if(c == Color.white.getRGB()) {
+						System.out.print(" ");
+						continue;
+					}
+					System.out.print("X");
+				}
+				System.out.println();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private static BufferedImage scale(BufferedImage sbi, int imageType, int dWidth, int dHeight, double fWidth, double fHeight) {
+	    BufferedImage dbi = null;
+	    if(sbi != null) {
+	        dbi = new BufferedImage(dWidth, dHeight, imageType);
+	        Graphics2D g = dbi.createGraphics();
+	        AffineTransform at = AffineTransform.getScaleInstance(fWidth, fHeight);
+	        g.drawRenderedImage(sbi, at);
+	    }
+	    return dbi;
+	}
 }
